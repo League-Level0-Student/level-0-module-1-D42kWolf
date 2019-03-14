3ca2e2874ef5eaf570afe1b22a78c0452ad30bdd
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-03-14 13:22:37 -0700

    Remarkable secretmessagebox

--- src/ifs_ints_and_loops/Remarkable.java
+++ src/ifs_ints_and_loops/Remarkable.java
@@ -5,14 +5,21 @@ import javax.swing.JOptionPane;
 public class Remarkable {
 	public static void main(String[] args) {
 		String message = JOptionPane.showInputDialog("What's your name?");
-		System.out.println(message); 
-		if(message.equals("Ayden")) {
-			JOptionPane.showMessageDialog(null,"Y0u funny boi");
-		
-		
-		
-		
-		
-		
-		
-}}
+		System.out.println(message);
+		String ayden = "Y0u funny boi";
+		if (message.equals("ayden")) {
+			JOptionPane.showMessageDialog(null, ayden);
+		} else if (message.equals("jacob")) {
+			String jacob = "Your special in your own way";
+
+			JOptionPane.showMessageDialog(null, jacob);
+		} else if (message.equals("carlos")) {
+			String carlos = "Look honeybager behide you";
+
+			JOptionPane.showMessageDialog(null, carlos);
+
+		}
+
+	}
+
+}
---
+++ src/ifs_ints_and_loops/secretmessagebox.java
@@ -0,0 +1,7 @@
+package ifs_ints_and_loops;
+
+import javax.swing.JOptionPane;
+
+public class secretmessagebox {
+ String password = JOptionPane.showMessageDialog(null, "Jigglypuff");
+}
