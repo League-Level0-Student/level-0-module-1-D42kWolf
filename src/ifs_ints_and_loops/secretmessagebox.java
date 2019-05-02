3f6443f1f1fc880e7c0f3187d170dcd4744269ea
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-05-02 13:24:13 -0700

    secretmessagebox

--- src/ifs_ints_and_loops/secretmessagebox.java
+++ src/ifs_ints_and_loops/secretmessagebox.java
@@ -2,6 +2,18 @@ package ifs_ints_and_loops;
 
 import javax.swing.JOptionPane;
 
-public class secretmessagebox {
- String password = JOptionPane.showMessageDialog(null, "Jigglypuff");
+public class secretmessagebox 
+	public static void main(String[] args) {
+	System.out.println(passcode);
+ String password ="Jigglypuff";
+JOptionPane.showInputDialog("Please type the secret message");
+ 
+String passcode = "";
+if (message.equals("ayden")) {
+	JOptionPane.showMessageDialog(null, ayden);
+} else if (message.equals("jacob")) {
+	String jacob = "Your special in your own way";
+ 
+ 
 }
+}}
