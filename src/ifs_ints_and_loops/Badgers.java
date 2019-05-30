fa202555b5abf4ccac3905b30aa492e40ea2a7c8
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-05-30 13:27:16 -0700

    Badgers

---
+++ src/ifs_ints_and_loops/Badgers.java
@@ -0,0 +1,17 @@
+package ifs_ints_and_loops;
+
+import javax.swing.JOptionPane;
+
+public class Badgers {
+	public static void main(String[] args) {
+		for (int i = 0; i < 2; i++) {
+			for (int b = 0; b < 12; b++) {
+				System.out.println("Badger");
+
+			}
+			for (int m = 0; m < 2; m++) {
+				System.out.println("mushrooms");
+			}
+		}
+	}
+}
--- src/ifs_ints_and_loops/Myages.java
+++ src/ifs_ints_and_loops/Myages.java
@@ -1,5 +1,11 @@
 package ifs_ints_and_loops;
-
 public class Myages {
+public static void main(String[] args) {
+	for(int i = 0; i<14; i++) {
+	System.out.println(i);
+		
+		
+	}
+}
 
 }
